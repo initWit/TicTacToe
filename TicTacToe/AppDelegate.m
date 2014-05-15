9aa460a7ebbf57078676d55efea26e4c90e05d47
initWit <figueras.robert@gmail.com>
2014-05-15 13:32:28 -0500

    first commit

---
+++ TicTacToe/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  TicTacToe
+//
+//  Created by Robert Figueras on 5/15/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <CheckMate/CheckMate.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [CheckMate initializeFramework:@[@"8a8a97f928c21b4ffa2142266e47adb9",@"d3006ae1b0e73d5213da0c7ca54c0af5"]];
+
+    return YES;
+}
+							
+
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Robert Figueras on 5/15/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
