c82c5e5b442e96613296ae878f6f03d5199b3833
initWit <figueras.robert@gmail.com>
2014-05-16 08:33:52 -0500

    Removed Checkmate Framework

--- TicTacToe/AppDelegate.m
+++ TicTacToe/AppDelegate.m
@@ -7,13 +7,13 @@
 //
 
 #import "AppDelegate.h"
-#import <CheckMate/CheckMate.h>
+//#import <CheckMate/CheckMate.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [CheckMate initializeFramework:@[@"8a8a97f928c21b4ffa2142266e47adb9",@"d3006ae1b0e73d5213da0c7ca54c0af5"]];
+//    [CheckMate initializeFramework:@[@"8a8a97f928c21b4ffa2142266e47adb9",@"d3006ae1b0e73d5213da0c7ca54c0af5"]];
 
     return YES;
 }
